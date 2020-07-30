104cc1721c77f056087cd98b4c19ec9670b7f2ab
Mart <mcpkmart@hotmail.nl>
2020-07-30 12:32:33 +0200

    Init commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace StoryGuardian.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
