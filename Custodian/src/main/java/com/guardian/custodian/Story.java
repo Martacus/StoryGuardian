87427a4e7132286a582e19fd893ca6f987c6db70
Mart <mcpkmart@hotmail.nl>
2020-09-13 22:20:18 +0200

    Changes before module testing

---
+++ Custodian/src/main/java/com/guardian/custodian/Story.java
@@ -0,0 +1,4 @@
+package com.guardian.custodian;
+
+public class Story {
+}
--- gaia/src/main/java/com/guardian/gaia/controllers/EntityController.java
+++ gaia/src/main/java/com/guardian/gaia/controllers/EntityController.java
@@ -1,4 +1,11 @@
 package com.guardian.gaia.controllers;
 
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class EntityController {
+
+
+
+
 }
